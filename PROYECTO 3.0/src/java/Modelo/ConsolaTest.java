74aaa4bed7735fea454a2d024ef8a6349eacbbef
frafariass <fra.fariass@alumnos.duoc.cl>
2019-06-22 04:13:48 -0400

    F

--- PROYECTO 3.0/src/java/Modelo/ConsolaTest.java
+++ PROYECTO 3.0/src/java/Modelo/ConsolaTest.java
@@ -17,29 +17,98 @@ public class ConsolaTest {
     {
         BD bd = new BD();
         
-      System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\FINAL\\proyecto-fixeado\\images\\tipo_producto\\tuercas.jpg",
-               "tipo_producto", "tipoprod_imagen", "104", "id_tipoprod"));
-      
-      
-              System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\PROYECTO 2.0\\images\\tipo_producto\\clavos.jpg",
-            "tipo_producto", "tipoprod_imagen", "101", "id_tipoprod"));
-               System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\PROYECTO 2.0\\images\\tipo_producto\\tornillos.jpg",
-              "tipo_producto", "tipoprod_imagen", "103", "id_tipoprod"));
-                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\PROYECTO 2.0\\images\\tipo_producto\\tuercas.jpg",
-            "tipo_producto", "tipoprod_imagen", "102", "id_tipoprod"));
-                                System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\PROYECTO 2.0\\images\\tipo_producto\\plagas.jpg",
-             "tipo_producto", "tipoprod_imagen", "107", "id_tipoprod"));
-                                    System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\PROYECTO 2.0\\images\\tipo_producto\\tuberia.jpg",
-               "tipo_producto", "tipoprod_imagen", "106", "id_tipoprod"));
-                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\PROYECTO 2.0\\images\\tipo_producto\\productos\\clavogrande.jpg",
-                "producto", "imagen", "10110100000000101", "id_producto"));
-                                       
-      String fechavenctest = "21062019";
-      String fechavenc = "";
-      fechavenc = fechavenctest.substring(0,2);
-        fechavenc = fechavenc + "-" + fechavenctest.substring(2,4);
-        fechavenc = fechavenc + "-" + fechavenctest.substring(4,fechavenctest.length());
-        System.out.println(fechavenc);
+      System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\cemento.jpg",
+               "tipo_producto", "tipoprod_imagen", "108", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\tabique.jpg",
+               "tipo_producto", "tipoprod_imagen", "109", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\alambres.jpg",
+               "tipo_producto", "tipoprod_imagen", "110", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\aislantes.jpg",
+               "tipo_producto", "tipoprod_imagen", "111", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\cadenas.jpg",
+               "tipo_producto", "tipoprod_imagen", "112", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\cuerdas.jpg",
+               "tipo_producto", "tipoprod_imagen", "113", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\acce construccion.jpg",
+               "tipo_producto", "tipoprod_imagen", "114", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\alicates.jpg",
+               "tipo_producto", "tipoprod_imagen", "115", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\dados.jpg",
+               "tipo_producto", "tipoprod_imagen", "116", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\desatornilladores.jpg",
+               "tipo_producto", "tipoprod_imagen", "117", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\serruchos.jpg",
+               "tipo_producto", "tipoprod_imagen", "118", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\martillos.jpg",
+               "tipo_producto", "tipoprod_imagen", "119", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\prensas.jpg",
+               "tipo_producto", "tipoprod_imagen", "120", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\set herramientas.jpg",
+               "tipo_producto", "tipoprod_imagen", "121", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\canalizacion.jpg",
+               "tipo_producto", "tipoprod_imagen", "122", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\alargadores.jpg",
+               "tipo_producto", "tipoprod_imagen", "123", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\temporizador.jpg",
+               "tipo_producto", "tipoprod_imagen", "124", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\prot electricistas.jpg",
+               "tipo_producto", "tipoprod_imagen", "125", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\cajas de distribucion.jpg",
+               "tipo_producto", "tipoprod_imagen", "126", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\enchufes.jpg",
+               "tipo_producto", "tipoprod_imagen", "127", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\pasacables.jpg",
+               "tipo_producto", "tipoprod_imagen", "128", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\pintura exterior.jpg",
+               "tipo_producto", "tipoprod_imagen", "129", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\pintura interior.png",
+               "tipo_producto", "tipoprod_imagen", "130", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\pintura en spray.jpg",
+               "tipo_producto", "tipoprod_imagen", "131", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\diluyente.jpg",
+               "tipo_producto", "tipoprod_imagen", "132", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\herramientas pintor.jpg",
+               "tipo_producto", "tipoprod_imagen", "133", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\barnices.jpg",
+               "tipo_producto", "tipoprod_imagen", "134", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\texturas muros.jpg",
+               "tipo_producto", "tipoprod_imagen", "135", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\extractores.jpg",
+               "tipo_producto", "tipoprod_imagen", "136", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\percheros.jpg",
+               "tipo_producto", "tipoprod_imagen", "137", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\flexible.jpg",
+               "tipo_producto", "tipoprod_imagen", "138", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\llave baño.jpg",
+               "tipo_producto", "tipoprod_imagen", "139", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\destapes.jpg",
+               "tipo_producto", "tipoprod_imagen", "140", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\griferia ducha.jpg",
+               "tipo_producto", "tipoprod_imagen", "141", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\fertilizantes.jpg",
+               "tipo_producto", "tipoprod_imagen", "142", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\maceteros.jpg",
+               "tipo_producto", "tipoprod_imagen", "143", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\cercos.jpg",
+               "tipo_producto", "tipoprod_imagen", "144", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\mangueras.jpg",
+               "tipo_producto", "tipoprod_imagen", "145", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\tijeras.jpg",
+               "tipo_producto", "tipoprod_imagen", "146", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\rastrillo.png",
+               "tipo_producto", "tipoprod_imagen", "147", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\ampolletas.jpg",
+               "tipo_producto", "tipoprod_imagen", "148", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\focos.jpg",
+               "tipo_producto", "tipoprod_imagen", "149", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\tubos.png",
+               "tipo_producto", "tipoprod_imagen", "150", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\guirnaldas.jpg",
+               "tipo_producto", "tipoprod_imagen", "151", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\pantalla.jpg",
+               "tipo_producto", "tipoprod_imagen", "152", "id_tipoprod"));
+                                       System.out.println(bd.insertarImagen("C:\\Users\\lordp\\Desktop\\portafolio\\proyecto-fixeado\\images\\tipo_producto\\barras.jpg",
+               "tipo_producto", "tipoprod_imagen", "153", "id_tipoprod"));
 
         
        
